b2026bb30c9ba544aad453d1946b487da7b780c8
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-07-28 10:23:45 -0700

    hidden letters are now rendered on browser

--- game.js
+++ game.js
@@ -4,21 +4,28 @@ function selectWord() {
     //randomly select a word 
     //us math to access index, display string at that index
     //number of letters rendered on hmtl 
-    //return length of word -> passed into -> hiddenWord()
 
     var wordBank = [ "cat", "hat", "bat", "spicy"];
+
     var wordIndex = Math.floor(Math.random() * wordBank.length);
-    var lengthOfWord = wordBank[wordIndex].length;
+    var selectedWord = wordBank[wordIndex];
 
-    document.getElementById("numOfLetters").innerHTML = "You word has " + lengthOfWord + " letters!";
-    
-    return lengthOfWord;
+    document.getElementById("numOfLetters").innerHTML = "You word has " + selectedWord.length + " letters!";
 
-}
+    //e/c letter in selectedWord is split into an individual string item 
+    //& is set to it's own array 
 
-function hiddeWord() {
-    //return word to html represented by underscores
-    console.log(lengthOfWord);
+    var splitWord = selectedWord.split("");
+
+    //render word in html represented by underscores 
+
+    function makeUnder(letter) {
+        var docTag = document.createElement('span');
+        docTag.innerHTML = " __ ";
+        document.body.appendChild(docTag);
+    }
+
+    splitWord.forEach(makeUnder);
 
 }
 
