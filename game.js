5c13e60489924f7933ca59599f873ec1b0518480
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-07-31 11:39:56 -0700

    using my game.js file to visually render _ for unguessed letters is not a good idea

--- game.js
+++ game.js
@@ -5,51 +5,54 @@ function selectWord() {
     //us math to access index, display string at that index
     //number of letters rendered on html 
 
-    var wordBank = [ "CAT", "HAT", "BAT", "SPICY"];
+    const wordBank = [ "CAT", "HAT", "BAT", "SPICY"];
 
-    var wordIndex = Math.floor(Math.random() * wordBank.length);
-    var selectedWord = wordBank[wordIndex];
+    let wordIndex = Math.floor(Math.random() * wordBank.length);
+    let selectedWord = wordBank[wordIndex];
+    //document.getElementById("numOfLetters").innerHTML = "You word has " + selectedWord.length + " letters!";
+    let gameWordArr = selectedWord.split("");
+    // gameWordArr.forEach(makeUnder);
 
-    document.getElementById("numOfLetters").innerHTML = "You word has " + selectedWord.length + " letters!";
+    // function makeUnder(letter) {
+    //     var docTag = document.createElement('span');
+    //     docTag.innerHTML = " _ ";
+    //     document.body.appendChild(docTag);
+    // }
 
-    //e/c letter in selectedWord is split into an individual string item 
-    //& is set to it's own array 
-
-    var splitWord = selectedWord.split("");
-
-    //render word in html represented by underscores 
-    //pass word (array) into your checkValue 
-
-    function makeUnder(letter) {
-        var docTag = document.createElement('span');
-        docTag.innerHTML = " __ ";
-        document.body.appendChild(docTag);
-    }
-
-    splitWord.forEach(makeUnder);
-
-    checkValue(splitWord);
+    return gameWordArr;
 
 }
+
+
+
+let gameWord = selectWord();
+console.log(gameWord);
 
 function restartGame() {
     location.reload();
 }
 
-function checkValue(gameWord, selectedLetter) {
-    console.log(gameWord);
-    //grab word
-    //compare letters in  word to user input
-    for (let i = 0; i < gameWord.length; i++) {
-        var hiddenLetter = gameWord[i];
-        if(hiddenLetter == selectedLetter) {
-            console.log("yes");
-        } else {
-            console.log("no");
-        }
-    }
-    //determine whether or user input is in selectedWord
+function checkValue(letter) {
+    console.log(letter)
 }
+
+console.log(selectWord.word);
+
+
+
+
+
+// is the letter selected by the user 
+// also a letter in the secret word 
+// if so, print yes, if no, print no 
+
+
+
+
+
+
+   
+
 
 
 
