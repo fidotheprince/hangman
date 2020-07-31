e430f9978c172b557c6a0f72f5b6e3b59cb9c665
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-07-31 12:07:12 -0700

    user guess can now correctly reference existence of letter in hidden word

--- game.js
+++ game.js
@@ -1,42 +1,40 @@
-
+//game.js ONLY handles the logic of the game 
 function selectWord() {
 
     //randomly select a word 
     //us math to access index, display string at that index
-    //number of letters rendered on html 
 
     const wordBank = [ "CAT", "HAT", "BAT", "SPICY"];
 
     let wordIndex = Math.floor(Math.random() * wordBank.length);
     let selectedWord = wordBank[wordIndex];
-    //document.getElementById("numOfLetters").innerHTML = "You word has " + selectedWord.length + " letters!";
     let gameWordArr = selectedWord.split("");
-    // gameWordArr.forEach(makeUnder);
-
-    // function makeUnder(letter) {
-    //     var docTag = document.createElement('span');
-    //     docTag.innerHTML = " _ ";
-    //     document.body.appendChild(docTag);
-    // }
 
     return gameWordArr;
 
 }
 
-
-
 let gameWord = selectWord();
 console.log(gameWord);
+
+function checkValue(guessedletter, word = gameWord) {
+
+    for (let i = 0; i < word.length; i++) {
+        let hiddenLetter = word[i];
+
+        if (hiddenLetter == guessedletter) {
+            console.log("correct");
+        } else {
+            console.log("incorrect")
+        }
+    }
+}
 
 function restartGame() {
     location.reload();
 }
 
-function checkValue(letter) {
-    console.log(letter)
-}
 
-console.log(selectWord.word);
 
 
 
