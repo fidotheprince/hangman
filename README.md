c22b3e3cb6f58233f02853b46c3845b6de443bbb
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-08-04 13:08:30 -0700

    Figuring out how to determine a win state for the game, utilized .splice, .map, .forEach to do so

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hangman
 Under construction ... 
+ 
+ 
--- game.js
+++ game.js
@@ -4,7 +4,7 @@ function selectWord() {
     //randomly select a word 
     //us math to access index, display string at that index
 
-    const wordBank = [ "CAT", "HAT", "BAT", "SPICY", "CCC"];
+    const wordBank = [ "CAT", "HAT", "BAT", "SPICY", "CCC", "FOREVER"];
 
     let wordIndex = Math.floor(Math.random() * wordBank.length);
     let selectedWord = wordBank[wordIndex];
@@ -14,58 +14,61 @@ function selectWord() {
 
 }
 
+//determined by body parts in the hang man 1 guess = 1 body part
+//evaluated array has to be held globally 
+//global state saves in memory every new item added to array 
+//since it is the same instance 
+
 let gameWord = selectWord();
 let totalLetters = gameWord.length;
+let winCount = 0;
 console.log(gameWord, totalLetters);
 
 
 
-//evaluated array has to be held globally 
 
-let evalArr = [];
-
-function compareValue(userLetter, hiddenWord = gameWord ) {
+function compareValue(inputLetter, hiddenWord = gameWord) {
     //iterate through existing array to compare input
-    for (let i = 0; i <= hiddenWord.length; i++) {
+    for (let i = 0; i <= hiddenWord.length - 1; i++) {
         let hiddenLetter = hiddenWord[i];
+        console.log(inputLetter, hiddenLetter);
 
-        if(userLetter == hiddenLetter) {
-            evalArr.push(userLetter);
+    //replace match with a boolean statement of true 
+        if (inputLetter === hiddenLetter) {
+            hiddenWord.splice(i, 1, true);
         } 
     }
 
-    console.log(evalArr);
-    //and index of hiddenWord
-    //determine a WIN or a LOSS potentially in this function
+    console.log(hiddenWord);
+    winState(hiddenWord);
+
 }
+
+//iterate through evalArr = if all items == true; you win
+function winState(evalArr, count = winCount) {
+
+  evalArr.forEach(checkWin);
+
+  function checkWin(item){
+
+    if (item === true) {
+        count++;
+    }
+    //winState might be able to handle to L as well
+  }
+
+  console.log(count);
+
+}
+
+
+
+
+
 
 function restartGame() {
     location.reload();
 }
-
-
-//let evalArr = gameWord.map(function());
-
-/*have function return value to new array if yes*/
-
-//maybe this array has to be held globablly
-//no just on the instance of letter
-// function checkValue(guessedLetter, word = gameWord) {
-
-//     word.forEach(evalState);
-
-//     function evalState(hiddenLetter) {
-//         if (hiddenLetter == guessedLetter) {
-//             let evalArray = [];
-//             evalArray.push(hiddenLetter);
-//         }
-//     }
-// }
-
-
-
-   
-
 
 
 
