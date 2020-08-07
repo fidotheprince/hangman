d35ca29ca5e1606f44f321c4f11e50dd840189b0
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-08-07 12:03:18 -0700

    still looking to determine loss state

--- game.js
+++ game.js
@@ -1,9 +1,21 @@
 //game.js ONLY handles the logic of the game 
+
+//determined by body parts in the hang man 1 guess = 1 body part
+//evaluated array has to be held globally 
+//global state saves in memory every new item added to array 
+//since it is the same instance 
+
+let gameWord = selectWord();
+let totalLetters = gameWord.length;
+let winCount = 0;
+let turnCount = 0;
+let noMatch = [];
+
+//randomly select a word 
+//us math to access index, display string at that index
+
 function selectWord() {
-
-    //randomly select a word 
-    //us math to access index, display string at that index
-
+   
     const wordBank = [ "CAT", "HAT", "BAT", "SPICY", "CCC", "FOREVER"];
 
     let wordIndex = Math.floor(Math.random() * wordBank.length);
@@ -14,21 +26,14 @@ function selectWord() {
 
 }
 
-//determined by body parts in the hang man 1 guess = 1 body part
-//evaluated array has to be held globally 
-//global state saves in memory every new item added to array 
-//since it is the same instance 
+function restartGame() {
+    location.reload();
+}
 
-let gameWord = selectWord();
-let totalLetters = gameWord.length;
-let winCount = 0;
-let letterBank = [];
-let maxGuessCount = 6;
-
-
+//display gameWord & total number of letters in it 
 console.log(gameWord, totalLetters);
 
-function compareValue(inputLetter, hiddenWord = gameWord, lossArr = letterBank) {
+function compareValue(inputLetter, hiddenWord = gameWord, noMatchArr = noMatch) {
 
     //iterate through existing array to compare input
     for (let i = 0; i <= hiddenWord.length - 1; i++) {
@@ -39,42 +44,68 @@ function compareValue(inputLetter, hiddenWord = gameWord, lossArr = letterBank) 
         if (inputLetter === hiddenLetter) {
             hiddenWord.splice(i, 1, true);
         } else {
-            lossArr.push(inputLetter);
-            //perhaps send input letter to a letter bank
-            //once letter banks are full # = maxGuessCount = loss
+            //if no match is found push a false value into the noMatchArr
+            noMatchArr.push(false);
         }
     }
 
-    console.log(lossArr);
-    console.log(hiddenWord);
     winState(hiddenWord);
+    addCount(hiddenWord, noMatchArr);
 
 }
 
-//iterate through evalArr = if all items == true; you win
+
+//iterate through evalArr = if all items are true; you win
 function winState(evalArr, countVic = winCount, lettersLeft = totalLetters) {
 
-  evalArr.forEach(checkWin);
+    evalArr.forEach(checkWin);
 
-  function checkWin(item){
+    function checkWin(item){
+      if (item === true) {
+          countVic++;
+          lettersLeft--; 
+      } 
+      return countVic; 
+    }
+  
+    console.log(countVic + " Count to Victory", lettersLeft + " Letters left");
+  
+}
 
-    if (item === true) {
-        countVic++;
-        lettersLeft--; 
-    } 
+//addCount evaluates the amount of guesses a players has
+//and if a user loses the game
+function addCount(matchArr = hiddenWord,  noLetterMatch = noMatchArr, turnsUsed = turnCount) {
 
-    return countVic; 
-    //winState might be able to handle to L as well
-  }
 
-  console.log(countVic + " Count to Victory", lettersLeft + " Letters left");
+    console.log(matchArr);
+    console.log(noLetterMatch);
+
+    if (matchArr.length === noLetterMatch.length) {
+
+        turnsUsed++;
+        noLetterMatch = [];
+        console.log(turnsUsed);
+    }
+
+   
+
+    //if the amount of true doesn't equal the amount of available letters 
+    //then don't add count 
+    //if the amount of howdy's DOES equal the amount of available letters 
+    //then no matches were found add to count; 
+    //everytime there is a match do
 
 }
 
 
-function restartGame() {
-    location.reload();
-}
+
+
+
+
+
+
+
+
 
 
 
