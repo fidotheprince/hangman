65581f5525f88da200b36b3f3fe50b64d72e409b
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-08-10 06:53:22 -0700

    I boiled down the app functionality to it's most basic form, very happy with the simplicity of the code.

--- game.js
+++ game.js
@@ -1,18 +1,16 @@
 //game.js ONLY handles the logic of the game 
 
-//determined by body parts in the hang man 1 guess = 1 body part
-//evaluated array has to be held globally 
-//global state saves in memory every new item added to array 
-//since it is the same instance 
-
+//guesses allowed determined by body parts in the hang man 1 guess = 1 body part
+//evaluated arrays have to be held globally 
+//global state saves in memory every new item added or removed to array since it is the same instance 
 let gameWord = selectWord();
 let totalLetters = gameWord.length;
 let guesses = [];
 let winArr = [];
 
+
 //randomly select a word 
 //us math to access index, display string at that index
-
 function selectWord() {
    
     const wordBank = [ "CAT", "HAT", "BAT", "SPICY", "CCC", "FOREVER"];
@@ -29,20 +27,19 @@ function restartGame() {
     location.reload();
 }
 
-//display gameWord & total number of letters in it 
+//display gameWord  
 console.log(gameWord);
 
-//add user input = placed in array regardless of YES or NO 
-//when array reaches certain number = loss 
-//if YES eliminate that letter from array 
-//when YES = number of letter in original word = WIN 
 
-
+//compareValue - evaluates user input and return indicates a win or a loss 
 function compareValue(inputLetter, hiddenWord = gameWord, guessBank = guesses, winCount = winArr) {
-
+    //add user input into guessBank(array) regardless of a correct or incorrect guess
     guessBank.push(inputLetter);
     hiddenWord.forEach(compare);
 
+    //if user input matches a hidden letter, remove input from the guessBank 
+    //preventing the total number of guesses (items in guessBank) from incrementing 
+    //if user input matches a hidden letter, add input to winCount(array)
     function compare(secretLetter) {
         if (secretLetter === inputLetter) {
             guessBank.splice(inputLetter, 1);
@@ -50,10 +47,12 @@ function compareValue(inputLetter, hiddenWord = gameWord, guessBank = guesses, w
         }
     }
 
-    if (guessBank.length === 3) {
+    //when guessBank reaches a certain number (of allowed guesses), the player loses 
+    if (guessBank.length === 7) {
         console.log("you lose");
     }
 
+    //when number of letters (items) in the winCount match the number of letters (items) in the hiddenWord, the player wins
     if( winCount.length === hiddenWord.length) {
         console.log("you win");
     }
