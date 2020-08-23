5b92c273678068cf8572c81cfca1b01490d39202
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-08-23 05:51:21 -0700

    code clean up

--- game.js
+++ game.js
@@ -1,14 +1,6 @@
 
 function VisualHandler() {
 
-    
-    //make object into an array.join// render dynamically 
-    //newObject with state set the instance before it 
-    //or remove property and value once matched 
-    //I only want one letter to show at a time
-    //potential send letter to as an array item and joing the item? 
-     //push elements to an array // make array string // and then render array 
-
     this.letterBlanks = function (obj) {
 
         let arrToStr = [];
@@ -22,24 +14,18 @@ function VisualHandler() {
             }
 
             document.getElementById('show').innerHTML = arrToStr.join(" ");
-          
         }     
      }
-    
-        //if value at letter key is false -> show " _ "
-        //else if value at letter key is true -> show " value (e.g. letter)"
-        //document.getElementById("show").innerHTML = gameLettersArr.join(" ");
 } 
 
-//this only has to sort an object once "key" = letter : "value" = boolean
-//in order to have the object update after every onclick event, we have to keep the state of that object globally 
 function orgObject() {
+
         this.flipKeyValue = function(obj) {
             let newObj = {};
             for(var key in obj) {
                 newObj[obj[key]] = false;
             }
-            //send to new obj
+            
             return newObj
         };      
 }
@@ -54,16 +40,8 @@ function UpdateObjState () {
             }
         }
             return obj
-        
     }
 }
-
-
-//object passed from here is ObjToDisplay(change this name) -> make word object 
-//creat object that switch value to true when there is a match  -> sort object 
-//this object passes the object with set values to -> change value and send to be redered 
-//your visualHandler 
-       
 
 function selectWord() {
    
@@ -123,19 +101,8 @@ const gameWord = new selectWord();
 let unSortedObj = Object.assign({}, gameWord);
 let sort = new orgObject();
 let sortedObj = sort.flipKeyValue(unSortedObj);
-//
 console.log(sortedObj);
 
 
 
 
-//     //
-//     this.guessResult = function(guess) {
-//         if (guess == true) {
-//             //potential render word in place of array 
-//             document.getElementById("testTwo").innerHTML = "You win";
-//         } else if (guess = false) {
-//             document.getElementById("testTwo").innerHTML = "You lose";
-//         }
-//     };
-// }
