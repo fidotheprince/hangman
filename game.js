97923ea49d037f7cc2bba8510618e4cb0c45010d
Alvaro Castro-Cid <alvarocastrocid@Alvaros-MBP.lan>
2020-08-31 06:15:28 -0700

    Mobile first design is complete and the app is finished.

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function VisualHandler() {
         let arrToStr = [];
 
         for (const letterProp in obj) {
-           
+
             if(obj[letterProp] === true) {
                 arrToStr.push(letterProp);
             } else if(obj[letterProp] === false) {
@@ -14,18 +14,48 @@ function VisualHandler() {
             }
 
             document.getElementById('show').innerHTML = arrToStr.join(" ");
+          
         }     
-     }
+     };
+
+     this.visualOutcome = function(win) {
+
+        if (win === true) {
+            document.getElementById("showTwo").innerHTML = "YOU WIN";
+        } else if (win === false) {
+            document.getElementById("showTwo").innerHTML = "YOU LOSE";
+        }
+    };
+
+    this.dispLetterCount = function(num) {
+        document.getElementById("dispTotalLett").innerHTML = "Letters: " + num;
+    }
+
+    this.bodyParts = function(arr) {
+        let count = arr.length;
+
+        if(count > 0 && count < 2) {
+            document.getElementById("rope").innerHTML = "<img src='images/rope.png'/>";
+        } else if(count > 1 && count < 3) {
+            document.getElementById("head").innerHTML = "<img src='images/head.png'/>";
+        } else if(count > 2 && count < 4) {
+            document.getElementById("torso").innerHTML = "<img src='images/torso.png'/>";
+        } else if (count > 3 && count < 5) {
+            document.getElementById("legs").innerHTML = "<img src='images/legs.png'/>";
+        } else if (count > 4 && count < 6) {
+            document.getElementById("feet").innerHTML =  "<img src='images/feet.png'/>";
+        } 
+    }
 } 
 
+//these function help construct the object model to be rendered in the VisualHandler 
 function orgObject() {
-
         this.flipKeyValue = function(obj) {
             let newObj = {};
             for(var key in obj) {
                 newObj[obj[key]] = false;
             }
-            
+
             return newObj
         };      
 }
@@ -41,17 +71,30 @@ function UpdateObjState () {
         }
             return obj
     }
-}
+}    
+
 
 function selectWord() {
-   
-    const wordBank = [ "CAT", "HAT", "BAT", "SPICY", "CCC", "FOREVER"];
+
+    let gameHud = new VisualHandler();
+    const wordBank = [ 
+        "CAT", "HAT", "BAT", "WATER", 
+        "SPICY", "BORN", "BIRTH", "THIRST",
+        "FRESH", "STRONG", "FACTS", "TRUE",
+        "DOVE", "POWERFUL", "CHAMPION", "FALSE",
+        "FAIR", "NOBLE", "PRINCE", "BEST", 
+        "FIRST", "PODIUM", "CROWN", "TECHNO"
+        ];
+
     let wordIndex = Math.floor(Math.random() * wordBank.length);
     let selectedWord = wordBank[wordIndex];
     let gameWordArr = selectedWord.split("");
+    gameHud.dispLetterCount(gameWordArr.length);
+
     return gameWordArr;
 
 }
+
 
 function compareValue(
     inputLetter, 
@@ -59,6 +102,8 @@ function compareValue(
     guessBank = guesses, 
     winCount = winArr, 
     wordObj = sortedObj) {
+    
+    document.getElementById(inputLetter).style.display = "none";
 
     guessBank.push(inputLetter);
     hiddenWord.forEach(compare);
@@ -69,6 +114,7 @@ function compareValue(
         let showToUser = new VisualHandler();
 
         if (secretLetter === inputLetter) {
+
             guessBank.splice(inputLetter, 1);
             winCount.push(inputLetter);
             evalObject = findInObj.matchedLetter(wordObj, inputLetter); 
@@ -77,15 +123,18 @@ function compareValue(
         }
     }
 
-    if (guessBank.length === 7) {
-        console.log("you lose");
-        console.log("Would you like to play again?")
+    renderResult = new VisualHandler();
+    renderResult.bodyParts(guessBank);
+
+    if (guessBank.length === 5) {
+    
+        renderResult.visualOutcome(false);
 
     }
     
     if( winCount.length === hiddenWord.length) {
-        console.log("you win");
-        console.log("Would you like to play again?")
+        renderResult.visualOutcome(true);
+
     }
 
 }
@@ -94,15 +143,17 @@ function restartGame() {
     location.reload();
 }
 
-
 let guesses = [];
 let winArr = [];
 const gameWord = new selectWord();
 let unSortedObj = Object.assign({}, gameWord);
 let sort = new orgObject();
 let sortedObj = sort.flipKeyValue(unSortedObj);
+
 console.log(sortedObj);
 
 
 
+
+    
 
